a608c9eb1f75870da98289aff7050e4178b5d138
Tung N Le <tung.le0319@gmail.com>
2023-01-04 17:48:55 -0700

    tung

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import "../node_modules/@mdi/font/css/materialdesignicons.css";
 
 import "../node_modules/bootstrap/dist/css";
-
+// import "../node_modules/@vueuse/motion"
 
 import { createApp } from "/vue";
 // @ts-ignore
 import App from "./App.vue";
 import { registerGlobalComponents } from "./registerGlobalComponents";
 import { router } from "./router";
-import { MotionPlugin } from "@vueuse/motion";
+import { MotionPlugin } from "../node_modules/@vueuse/motion";
 // Vuetify
 
 import Particles from "vue3-particles";

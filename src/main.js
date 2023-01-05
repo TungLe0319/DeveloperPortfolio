1627e8d0805373935e087fcde317e204c1731c92
Tung N Le <tung.le0319@gmail.com>
2023-01-04 18:31:12 -0700

    failure

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
-// import "../node_modules/@mdi/font/css/materialdesignicons.css";
-
-// import "../node_modules/bootstrap/dist/css";
-// import "../node_modules/@vueuse/motion"
-
-import { createApp } from "/vue";
+import "@mdi/font/css/materialdesignicons.css";
+import "bootstrap";
+import { createApp } from "vue";
 // @ts-ignore
 import App from "./App.vue";
 import { registerGlobalComponents } from "./registerGlobalComponents";
 import { router } from "./router";
-import { MotionPlugin } from "../node_modules/@vueuse/motion";
+import { MotionPlugin } from "@vueuse/motion";
 // Vuetify
 
+import Particles from "vue3-particles";
 
-// import Particles from '../node_modules/vue3-particles/dist/index.js'
 const root = createApp(App);
 registerGlobalComponents(root);
 
-root.use(router).use(MotionPlugin).mount("#app");
+root.use(router).use(MotionPlugin).use(Particles).mount("#app");
